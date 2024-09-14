8c069ea821748352439178643732020303626d94
Shivansh-22866 <shivanshp0418@gmail.com>
2024-09-14 23:09:08 +0530

    Checkout working, now go to post-payment pages

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/(.*)']);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
-    auth().protect()
+    auth().protect();
   }
-})
+});
 
 export const config = {
   matcher: [
+    // Match all routes except those starting with /api and static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    '/(api|trpc)(.*)', // Ensure that API and trpc routes are matched
   ],
 };
